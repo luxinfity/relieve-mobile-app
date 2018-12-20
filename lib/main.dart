f5ec45ffd6513a1f81fd2f7f9b494997717c8d3a
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-20 10:00:11 +0700

    relieve id name

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Relieve ID',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Relieve ID Home Page'),
     );
   }
 }
