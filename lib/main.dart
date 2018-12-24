ae9c3b974a3a8ecb58ce147bb501c96fda10526e
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-24 14:24:00 +0700

    use 4 spaces

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,19 @@ import 'res/color.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Relieve ID',
-      theme: ThemeData(
-        primaryColor: AppColor.colorPrimary,
-        primaryColorDark: AppColor.colorPrimaryDark,
-        accentColor: AppColor.colorAccent,
-        backgroundColor: Colors.white,
-        fontFamily: CircularStdFont.name,
-      ),
-      home: MyHomePage(title: 'Relieve ID Home Page'),
-    );
-  }
+    // This widget is the root of your application.
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Relieve ID',
+            theme: ThemeData(
+                primaryColor: AppColor.colorPrimary,
+                primaryColorDark: AppColor.colorPrimaryDark,
+                accentColor: AppColor.colorAccent,
+                backgroundColor: Colors.white,
+                fontFamily: CircularStdFont.name,
+            ),
+            home: MyHomePage(title: 'Relieve ID Home Page'),
+        );
+    }
 }
