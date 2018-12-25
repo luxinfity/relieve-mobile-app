2ffe884b8feb4313f7002c4d652757a23819edd6
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-25 13:59:15 +0700

    component in boarding home

--- lib/screen/boarding/boarding_home.dart
+++ lib/screen/boarding/boarding_home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
 import '../../res/image.dart';
+import '../../res/color.dart';
 
 class BoadingPage extends StatelessWidget {
     final String title;
@@ -7,20 +11,51 @@ class BoadingPage extends StatelessWidget {
     BoadingPage({Key key, this.title}) : super(key: key);
 
     @override
-    Widget build(BuildContext context) {        
-        return Scaffold(
-            appBar: AppBar(
-            // Here we take the value from the MyHomePage object that was created by
-            // the App.build method, and use it to set our appbar title.
-                title: Text(title),
-            ),
-            body: Column(
-                children: <Widget>[
-                    Image.network(
-                        RemoteImage.boardingHome,                                             
-                    ),
-                ],
-            ),
+    Widget build(BuildContext context) {
+        SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+            statusBarColor: AppColor.colorPrimaryDark
+        ));
+        return Scaffold (            
+            body: SafeArea(
+                child: ListView(
+                    children: <Widget>[                        
+                        Image.network(
+                            RemoteImage.boardingHome,                                             
+                        ),
+                        RaisedButton(
+                            child: Text('Login Now'),
+                            elevation: 0.5,
+                            highlightElevation: 0.5,
+                            
+                            onPressed: () {
+
+                            },
+                        ),
+                        RaisedButton.icon(
+                            icon: SvgPicture.asset('images/google.svg'),
+                            label: Text('Sign In With Google'),
+                            elevation: 0.5,
+                            highlightElevation: 0.5,
+                            
+                            onPressed: () {
+
+                            },
+                        ),
+                        Row (
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                                Text('Don’t have an account ?'),
+                                FlatButton(
+                                    child: Text('Register Here'),
+                                    onPressed: () {
+
+                                    },
+                                )
+                            ],
+                        )
+                    ],
+                ),
+            ) 
         );
     }
 }
