b782e450ae87b1b29840c1b2ad88559d192301f2
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2018-12-28 01:17:10 +0700

    update image and login screen

--- lib/screen/boarding/boarding_login.dart
+++ lib/screen/boarding/boarding_login.dart
@@ -14,8 +14,8 @@ class BoardingLogin extends StatelessWidget {
     }
 
     @override
-    Widget build(BuildContext context) {  
-        final EdgeInsets padding = MediaQuery.of(context).padding;   
+    Widget build(BuildContext context) {
+        final EdgeInsets padding = MediaQuery.of(context).padding;
         return Scaffold(
             backgroundColor: Colors.white,
             body: Column(
@@ -29,106 +29,131 @@ class BoardingLogin extends StatelessWidget {
                         icon: SvgPicture.asset('images/back_arrow.svg', height: 26),
                         onPressed: () => onBackPressed(context),
                     ),
-                    ThemedTitle(
-                        title: "Login Now",
-                        subtitle: "Please login to continue using our app"
-                    ),
-                    Container(height: Dimen.x18),
                     Expanded(
-                        child: Image.network(
-                            RemoteImage.boardingLogin,
-                            width: double.infinity,
-                        ),
-                    ),
-                    Container(
-                        width: double.infinity,
-                        margin: EdgeInsets.only(
-                            top: Dimen.x21,
-                            left: Dimen.x16,
-                            right: Dimen.x16
-                        ),
-                        child: TextFormField(
-                            decoration: InputDecoration(
-                                hintText: 'Username',
-                            ),
-                            maxLines: 1,
-                        ),
-                    ),
-                    Container(height: Dimen.x8),
-                    Container(
-                        width: double.infinity,
-                        margin: EdgeInsets.only(
-                            // top: Dimen.x21,
-                            left: Dimen.x16,
-                            right: Dimen.x16
-                        ),
-                        child: TextFormField(
-                            decoration: InputDecoration(
-                                hintText: 'Password',                                
-                            ),
-                            obscureText: true,
-                            maxLines: 1,
-                        ),
-                    ),
-                    Container(height: Dimen.x16),
-                    Row (
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                            Text('Don’t have an account ?',
-                                style: CircularStdFont.getFont(
-                                    size: Dimen.x14,
-                                    style: CircularStdFontStyle.Book
-                                ).apply(
-                                    color: AppColor.colorTextGrey
-                                )
-                            ),
-                            FlatButton(
-                                child: Text('Register Here',
-                                    style: CircularStdFont.getFont(
-                                        size: Dimen.x14,
-                                        style: CircularStdFontStyle.Book
-                                    ).apply(
-                                        color: AppColor.colorPrimary
-                                    )
-                                ),
-                                onPressed: () {
-
-                                },
-                            )
-                        ],
-                    ),
-                    Container(
-                        width: double.infinity,
-                        margin: EdgeInsets.only(
-                            top: Dimen.x21,
-                            left: Dimen.x16,
-                            right: Dimen.x16
-                        ),
-                        child: RaisedButton(
-                            child: Text('Login',
-                                style: CircularStdFont.getFont(
-                                    size: Dimen.x14,
-                                    style: CircularStdFontStyle.Medium
-                                ).apply(
-                                    color: Colors.white
-                                ),
-                            ),
-                            color: AppColor.colorPrimary,
-                            elevation: 1,
-                            highlightElevation: 1,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(4)
-                            ),
+                        child: ListView (
                             padding: EdgeInsets.only(
-                                top: Dimen.x16,
-                                bottom: Dimen.x16,
+                                left: padding.left,
+                                right: padding.right
                             ),
-                            onPressed: () {
-                                
-                            },
+                            children: <Widget>[
+                                ThemedTitle(
+                                    title: "Login Now",
+                                    subtitle: "Please login to continue using our app"
+                                ),
+                                SizedBox(
+                                    child: Image.network(
+                                        RemoteImage.boardingLogin,
+                                    ),
+                                    width: double.infinity,
+                                ),
+                                Container(
+                                    width: double.infinity,
+                                    margin: EdgeInsets.only(
+                                        left: Dimen.x16,
+                                        right: Dimen.x16
+                                    ),
+                                    child: TextFormField(
+                                        decoration: InputDecoration(
+                                            labelText: 'Username',
+                                        ),
+                                        maxLines: 1,
+                                    ),
+                                ),
+                                Container(
+                                    width: double.infinity,
+                                    margin: EdgeInsets.only(
+                                        top: Dimen.x6,
+                                        left: Dimen.x16,
+                                        right: Dimen.x16,
+                                        bottom: Dimen.x6,
+                                    ),
+                                    child: TextFormField(
+                                        decoration: InputDecoration(
+                                            labelText: 'Password',
+                                        ),
+                                        obscureText: true,
+                                        maxLines: 1,
+                                    ),
+                                ),
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: <Widget>[
+                                        FlatButton(
+                                            child: Text('Forgot Password?',
+                                                style: CircularStdFont.getFont(
+                                                    size: Dimen.x14,
+                                                    style: CircularStdFontStyle.Book
+                                                ).apply(
+                                                    color: AppColor.colorPrimary
+                                                )
+                                            ),
+                                            onPressed: () {
+
+                                            },
+                                        ),
+                                    ],
+                                ),
+                                Container(
+                                    width: double.infinity,
+                                    margin: EdgeInsets.only(
+                                        top: Dimen.x6,
+                                        left: Dimen.x16,
+                                        right: Dimen.x16
+                                    ),
+                                    child: RaisedButton(
+                                        child: Text('Login',
+                                            style: CircularStdFont.getFont(
+                                                size: Dimen.x14,
+                                                style: CircularStdFontStyle.Medium
+                                            ).apply(
+                                                color: Colors.white
+                                            ),
+                                        ),
+                                        color: AppColor.colorPrimary,
+                                        elevation: 1,
+                                        highlightElevation: 1,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(4)
+                                        ),
+                                        padding: EdgeInsets.only(
+                                            top: Dimen.x16,
+                                            bottom: Dimen.x16,
+                                        ),
+                                        onPressed: () {
+
+                                        },
+                                    ),
+                                ),
+                                Container(height: Dimen.x12),
+                                Row (
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                        Text('Don’t have an account ?',
+                                            style: CircularStdFont.getFont(
+                                                size: Dimen.x14,
+                                                style: CircularStdFontStyle.Book
+                                            ).apply(
+                                                color: AppColor.colorTextGrey
+                                            )
+                                        ),
+                                        FlatButton(
+                                            child: Text('Register Here',
+                                                style: CircularStdFont.getFont(
+                                                    size: Dimen.x14,
+                                                    style: CircularStdFontStyle.Book
+                                                ).apply(
+                                                    color: AppColor.colorPrimary
+                                                )
+                                            ),
+                                            onPressed: () {
+
+                                            },
+                                        )
+                                    ],
+                                ),
+                            ],
                         ),
-                    ),
-                    Container(height: Dimen.x24),
+                    ),                    
                 ],
             ),
         );
