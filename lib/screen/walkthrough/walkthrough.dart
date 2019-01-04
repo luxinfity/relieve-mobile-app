62e49f34c3394072fb2f29395efaa163c398a962
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2019-01-05 02:45:53 +0700

    add walkthrough button

--- lib/screen/walkthrough/walkthrough.dart
+++ lib/screen/walkthrough/walkthrough.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../dashboard/dashboard.dart';
+import '../../res/color.dart';
 import '../../widget/relieve_scaffold.dart';
+import '../../widget/item/standard_button.dart';
 
 class WalkthroughScreen extends StatefulWidget {
   WalkthroughScreen({Key key}) : super(key: key);
@@ -9,21 +12,38 @@ class WalkthroughScreen extends StatefulWidget {
   _WalkthroughScreenState createState() => _WalkthroughScreenState();
 }
 
+const WALKTHROUGH_SIZE = 4;
+
 class _WalkthroughScreenState extends State<WalkthroughScreen> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter += 1;
-    });
+  void _moveToNext(BuildContext context) {
+    if (_counter < WALKTHROUGH_SIZE) {
+      setState(() {
+        _counter += 1;
+      });
+    } else {
+      Navigator.push(context, MaterialPageRoute(builder: (buikder) => DashboardScreen()));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return RelieveScaffold(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      hasBackButton: true,
       childs: <Widget>[
-        Text('data')
+        Text(_counter.toString()),
+        buildActionButton(context)
       ],
     );
+  }
+
+  StandardButton buildActionButton(BuildContext context) {
+    return StandardButton(
+        text: (_counter < WALKTHROUGH_SIZE) ? 'Mengerti' : 'Ayo mulai!',      
+        backgroundColor: AppColor.colorPrimary,
+        buttonClick: () => _moveToNext(context),
+      );
   }
 }
