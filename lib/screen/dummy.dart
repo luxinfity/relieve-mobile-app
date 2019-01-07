9826903bd7d8cdb840623694280e44cf420a6629
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2019-01-07 19:55:23 +0700

    dummy page

--- lib/screen/dummy.dart
+++ lib/screen/dummy.dart
@@ -3,7 +3,35 @@ import 'package:flutter/material.dart';
 class DummyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              title: Text('Alkbae'),
+              flexibleSpace: FlexibleSpaceBar(
+                  // centerTitle: true,
+                  // title: Text("Collapsing Toolbar",
+                  //     style: TextStyle(
+                  //       color: Colors.white,
+                  //       fontSize: 16.0,
+                  //     )),
+                  // collapseMode: CollapseMode.parallax,
+                  background: Image.network(
+                    "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                    fit: BoxFit.cover,
+                  )
+                ),
+            ),
+          ];
+        },
+        body: Center(
+          child: Text("Sample Text"),
+        ),
+      ),
+    );
   }
 }
