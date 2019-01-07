4d69dd7a4fbbc56794915cf37d1598267660906c
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2019-01-07 19:21:36 +0700

    add dummy

---
+++ lib/screen/dummy.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DummyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
