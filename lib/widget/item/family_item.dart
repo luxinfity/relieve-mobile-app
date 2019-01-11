e93c85b1895c96a714f362fd15a0ed21147a7a41
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2019-01-12 02:59:20 +0700

    factory for family item

--- lib/widget/item/family_item.dart
+++ lib/widget/item/family_item.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
+enum FamilyItemType { Normal, Empty, Add }
+
+enum FamilyItemHealth { Fine, Bad, None }
+
 class FamilyItem extends StatelessWidget {
+  final FamilyItemType type;
+  final FamilyItemHealth health;
+  final String fullName;
+  final String imageUrl;
+
+  const FamilyItem({
+    Key key,
+    this.type,
+    this.health,
+    this.fullName,
+    this.imageUrl,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text('Alif'),
     );
+  }
+
+  static FamilyItem empty() {
+    return FamilyItem();
+  }
+
+  static FamilyItem add() {
+    return FamilyItem();
+  }
+
+  static FamilyItem normal() {
+    return FamilyItem();
   }
 }
