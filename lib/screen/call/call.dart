fbe0a474215a0af786fc6ea3a76485627874a7e3
Muhammad Alif Akbar <muh.alifgiant@gmail.com>
2019-01-18 23:13:48 +0700

    setup sliver padding

--- lib/screen/call/call.dart
+++ lib/screen/call/call.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../res/res.dart';
+import '../../network/model/family.dart';
 import '../../widget/relieve_scaffold.dart';
 import '../../widget/item/title.dart';
 import '../../widget/item/family_item.dart';
@@ -53,19 +54,27 @@ class CallScreen extends StatelessWidget {
                   )
                 ]),
               ),
-              SliverGrid.count(
-                crossAxisCount: 2,
-                children: <Widget>[
-                  Container(
-                    color: AppColor.colorAccent,
-                  ),
-                  Container(
-                    color: AppColor.colorDanger,
-                  ),
-                  Container(
-                    color: AppColor.colorEmptyChip,
-                  ),
-                ],
+              SliverPadding(
+                padding: EdgeInsets.all(6),
+                sliver: SliverGrid.count(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2,
+                  crossAxisSpacing: 6,
+                  mainAxisSpacing: 6,
+                  children: <Widget>[
+                    _buildButton(
+                      LocalImage.ic_user,
+                      'Profil dan password',
+                      axis: Axis.vertical,
+                    ),
+                    Container(
+                      color: AppColor.colorDanger,
+                    ),
+                    Container(
+                      color: AppColor.colorEmptyChip,
+                    ),
+                  ],
+                ),
               ),
               SliverList(
                 delegate: SliverChildListDelegate(<Widget>[
@@ -84,6 +93,41 @@ class CallScreen extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildButton(
+    LocalImage icon,
+    String title, {
+    Axis axis = Axis.horizontal,
+    bool isExit = false,
+  }) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: Dimen.x12, vertical: Dimen.x18),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: isExit ? AppColor.colorDanger : AppColor.colorEmptyChip,
+        ),
+        color: Colors.green,
+        borderRadius: BorderRadius.circular(Dimen.x6),
+      ),
+      child: Wrap(
+        direction: axis,
+        spacing: (axis == Axis.horizontal) ? Dimen.x12 : Dimen.x6,
+        children: <Widget>[
+          icon.toSvg(
+            width: Dimen.x18,
+            color: isExit ? AppColor.colorDanger : null,
+          ),
+          Text(
+            title,
+            style: CircularStdFont.book.getStyle(
+              size: Dimen.x12,
+              color: isExit ? AppColor.colorDanger : AppColor.colorTextBlack,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
