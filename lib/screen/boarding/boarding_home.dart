c1e5b43ff3f1b2ca26ee4cc510939ff119369dee
alifgiant <muh.alifgiant@gmail.com>
2019-03-16 22:25:39 +0700

    fix error import

--- lib/screen/boarding/boarding_home.dart
+++ lib/screen/boarding/boarding_home.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:relieve_app/res/res.dart';
+import 'package:relieve_app/screen/boarding/boarding_login.dart';
 import 'package:relieve_app/screen/boarding/components/boarding_register_here.dart';
+import 'package:relieve_app/screen/register/boarding_register.dart';
 import 'package:relieve_app/screen/register/register.dart';
 import 'package:relieve_app/screen/walkthrough/walkthrough.dart';
+import 'package:relieve_app/utils/common_utils.dart';
 import 'package:relieve_app/widget/item/standard_button.dart';
 import 'package:relieve_app/widget/item/title.dart';
 import 'package:relieve_app/utils/preference_utils.dart';
+import 'package:relieve_app/widget/relieve_scaffold.dart';
 
 class BoardingHomeScreen extends StatelessWidget {
   BoardingHomeScreen({Key key}) : super(key: key);
