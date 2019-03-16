d6cfbd3c0c4724fcaf355c752a35f1a5a390836b
alifgiant <muh.alifgiant@gmail.com>
2019-03-16 21:48:13 +0700

    safely add api key to git

---
+++ setAPIkey.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# DOWNLOAD env Variable from 
+# https://drive.google.com/drive/folders/1FN2ojK1K2lT6PTim3QDCLn187oRwFRgP
+# run that script will also invoke this script
+
+# Make sure you has the key before run this script
+if [ ! -z "$ANDROID_API_KEY" ]; then
+    echo "ANDROID_API_KEY is empty"
+    exit 1
+elif [ ! -z "$IOS_API_KEY" ]; then
+    echo "IOS_API_KEY is empty"
+    exit 2
+fi
+
+sed -i '' "s/ANDROID_API_KEY/$ANDROID_API_KEY/g" android/app/google-services.json
+sed -i '' "s/ANDROID_API_KEY/$ANDROID_API_KEY/g" android/app/src/main/AndroidManifest.xml
+sed -i '' "s/IOS_API_KEY/$IOS_API_KEY/g" ios/Runner/GoogleService-Info.plist
+
+exit 0
