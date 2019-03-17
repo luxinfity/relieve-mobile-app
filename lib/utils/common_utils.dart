697e5e4a08c0100da657ef444a39b15346425949
alifgiant <muh.alifgiant@gmail.com>
2019-03-17 20:10:07 +0700

    load static map as image

--- lib/utils/common_utils.dart
+++ lib/utils/common_utils.dart
@@ -8,17 +8,17 @@ void defaultBackPressed(BuildContext context) {
   Navigator.pop(context);
 }
 
-final GoogleSignIn googleSignInScope = GoogleSignIn(
-  scopes: [
-    'email',
-    'https://www.googleapis.com/auth/contacts.readonly',
-  ],
-);
-
-String getApiKey(BuildContext context) {
+String getGoogleApiKey(BuildContext context) {
   if (Theme.of(context).platform == TargetPlatform.iOS) {
     return "IOS_API_KEY";
   } else {
     return "ANDROID_API_KEY";
   }
 }
+
+final GoogleSignIn googleSignInScope = GoogleSignIn(
+  scopes: [
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
