26087ba1fcb5d466b117f462ba087ea9877143ca
alifgiant <muh.alifgiant@gmail.com>
2019-06-04 22:28:21 +0800

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +9,16 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
   ) -> Bool {
+    
     GMSServices.provideAPIKey("IOS_API_KEY")
+    
+    // Use Firebase library to configure APIs
+    let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")!
+    let options = FirebaseOptions(contentsOfFile: filePath)
+    FirebaseApp.configure(options: options!)
+
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,2 +1,3 @@
-export './source/api/base.dart';
+export 'package:relieve_app/service/source/api/base.dart';
 export 'package:relieve_app/service/source/location/location.dart';
+export 'package:relieve_app/service/source/firebase/base.dart';
