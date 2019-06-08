b145500095208d30c81e4bca5b5843278c8101c3
alifgiant <muh.alifgiant@gmail.com>
2019-06-08 22:50:58 +0800

    edit snackbar

---
+++ lib/widget/common/relieve_snackbar.dart
@@ -0,0 +1,48 @@
+import 'package:flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:relieve_app/res/res.dart';
+
+abstract class RelieveSnackBar {
+  static void show(
+    BuildContext context,
+    String text, {
+    String buttonText,
+    Color buttonTextColor,
+    Color backgroundColor,
+    VoidCallback onClick,
+  }) {
+    Flushbar flush;
+    flush = Flushbar(
+      flushbarStyle: FlushbarStyle.FLOATING,
+      aroundPadding:
+          EdgeInsets.symmetric(horizontal: Dimen.x16, vertical: Dimen.x16),
+      backgroundColor:
+          (backgroundColor == null) ? AppColor.colorTextBlack : backgroundColor,
+      message: text,
+      mainButton: (buttonText != null)
+          ? FlatButton(
+              child: Text(
+                buttonText,
+                style: CircularStdFont.medium.getStyle(
+                    size: Dimen.x14,
+                    color: (buttonTextColor == null)
+                        ? AppColor.colorAccent
+                        : buttonTextColor),
+              ),
+              onPressed: () {
+                if (onClick != null) onClick();
+                flush?.dismiss(true);
+              },
+            )
+          : null,
+      duration: Duration(seconds: 4),
+      borderRadius: Dimen.x8,
+    )..show(context);
+  }
+
+  /// behaviour not tested yet
+  static void dismiss(BuildContext context) {
+    // TODO: test behavior
+    Navigator.pop(context);
+  }
+}
